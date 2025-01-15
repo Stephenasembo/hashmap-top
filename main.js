9d1839c74ff819affc40afb8716d9ab3438d1080
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-15 23:51:03 +0300

    Use Provided Hash Function For Hashing

--- main.js
+++ main.js
@@ -3,5 +3,12 @@ class Hashmap {
     this.loadFactor = null;
     this.capacity = null;
   }
-  
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+    return hashCode;
+  }
 }
