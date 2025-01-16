822f77d21405e1cf5ca446588f9c0d95cf86d745
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-16 09:38:12 +0300

    Use A Linked List To Avoid Collisions

--- main.js
+++ main.js
@@ -14,12 +14,46 @@ class Hashmap {
   }
   set(key, value) {
     let hashCode = this.hash(key);
-    this.array[hashCode] = [key, value];
+    let newNode = Node(key, value);
+    // Check if the spot is occupied
+    if (!this.array[hashCode]) {
+      let head = null;
+      head = newNode;
+      this.array[hashCode] = head;
+    } else {
+      // Compare keys and overwrite value if they are equal
+      if (this.array[hashCode].key === key) {
+        this.array[hashCode].value = value;
+      } else {
+        // Append the new value as a linked list in an array
+        let lastNode = this.findLastNode(this.array[hashCode]);
+        if (lastNode === null) {
+          lastNode = newNode;
+        } else {
+          lastNode.nextNode = newNode;
+        }
+      }
+    }
+  }
+  findLastNode(node) {
+    if (node === null) {
+      return node;
+    }
+    if (node.nextNode === null) {
+      return node;
+    }
+    this.findLastNode(node.nextNode);
   }
 }
 
+function Node(key, value, node = null) {
+  return {
+    key: key,
+    value: value,
+    nextNode: node,
+  }
+}
 const test = new Hashmap();
-test.set('hello', 'mark');
-test.set('bye', 'stephen')
-test.set('fruit', 'banana')
+test.set('rama', 'mark');
+test.set('sita', 'stephen')
 console.log(test.array);
