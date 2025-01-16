f08214eeffbe35d8f3ffbab61f29ebefc44d433e
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-16 14:51:20 +0300

    Use The Provided Hash Function

--- hashset.js
+++ hashset.js
@@ -1,5 +1,17 @@
 class Hashmap {
-
+  constructor() {
+    this.loadFactor = null;
+    this.capacity = 16;
+    this.array = [];
+  }
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+    return hashCode;
+  }
 }
 
 class Node {
